18b0f87a247226342fd624f27fa2df747391540e
koorochka <artemij@koorochka.ru>
2018-08-12 13:59:12 +0300

    Player Join Event Sample (PlayerJoinEvent e)

--- src/intel/events/Handler.java
+++ src/intel/events/Handler.java
@@ -1,7 +1,20 @@
 package intel.events;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Handler implements Listener{
+
+    @EventHandler
+    public void j(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        String[] mesages = new String[3];
+        mesages[2] = "Hello!";
+        mesages[1] = "Your meage";
+        mesages[0] = "Zashell na servser";
+        p.sendMessage(mesages);
+    }
 
 }
