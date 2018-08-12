1fe8e3b36c523e4eec039ba8d64271abc08bd60e
koorochka <artemij@koorochka.ru>
2018-08-12 13:17:40 +0300

    Start point

---
+++ src/intel/events/IntelEvents.java
@@ -0,0 +1,12 @@
+package intel.events;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IntelEvents extends JavaPlugin{
+
+    @Override
+    public void onEnable(){
+        getLogger().info("Intellije IDEA plugin");
+    }
+
+}
